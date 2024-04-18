cdd0f2425abdbe51c8f87d11f49c2bcdc4844916
elodie ALB <elodie.albares0201@gmail.com>
2024-04-18 12:05:52 +0200

    (add) loueurEntity dans le module

--- backend_location/src/loueurs/loueurs.module.ts
+++ backend_location/src/loueurs/loueurs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LoueursService } from './loueurs.service';
 import { LoueursController } from './loueurs.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoueurEntity } from './loueur.entity/loueur.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([LoueurEntity])],
   providers: [LoueursService],
   controllers: [LoueursController]
 })
