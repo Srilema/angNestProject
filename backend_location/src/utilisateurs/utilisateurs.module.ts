929f7ab6d11b89d6286e8b54009e0d780d148546
elodie ALB <elodie.albares0201@gmail.com>
2024-04-18 12:06:23 +0200

    (add) utilisateurEntity dans le module

--- backend_location/src/utilisateurs/utilisateurs.module.ts
+++ backend_location/src/utilisateurs/utilisateurs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UtilisateursService } from './utilisateurs.service';
 import { UtilisateursController } from './utilisateurs.controller';
+import { UtilisateurEntity } from './utilisateur.entity/utilisateur.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UtilisateurEntity])],
   providers: [UtilisateursService],
   controllers: [UtilisateursController]
 })
