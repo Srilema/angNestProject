c326f50c88823bed795a8f7e4f4a898e845adcf0
elodie ALB <elodie.albares0201@gmail.com>
2024-04-24 11:34:19 +0200

    (add) getLocations .ts

--- frontend/src/app/location/location.component.ts
+++ frontend/src/app/location/location.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocationService } from '../service/location/location.service';
 
 @Component({
   selector: 'app-location',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './location.component.css'
 })
 export class LocationComponent {
+  locations: any
 
+  constructor(private locationService: LocationService) {
+  }
+  ngOnInit() {
+    let locationData = this.locationService.getLocations();
+    locationData.subscribe(res=>this.locations=res);
+    console.log('locations : ',this.locations);
+  }
 }
+
