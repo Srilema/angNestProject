87f396db652faf27736da477cfb36c4d79ba143e
elodie ALB <elodie.albares0201@gmail.com>
2024-04-24 14:53:48 +0200

    (add) formModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,18 +8,22 @@ import { AccueilComponent } from './accueil/accueil.component';
 import { RouterModule } from '@angular/router';
 import { AddLocationComponent } from './add-location/add-location.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LocationsDetailsComponent } from './locations-details/locations-details.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LocationComponent,
     AccueilComponent,
-    AddLocationComponent
+    AddLocationComponent,
+    LocationsDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
