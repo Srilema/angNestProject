2936a34b8d5959040291566a871e7ef72112af37
elodie ALB <elodie.albares0201@gmail.com>
2024-04-26 18:32:10 +0200

    creation-compte

--- frontend/src/app/creation-compte/creation-compte.component.ts
+++ frontend/src/app/creation-compte/creation-compte.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UtilisateurService } from '../service/utilisateur/utilisateur.service';
 
 @Component({
   selector: 'app-creation-compte',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class CreationCompteComponent {
 
+  constructor(private utilisateurService: UtilisateurService){}
+
+  submit(data: any) {
+    data.role="BAILLEUR";
+    console.log(data);
+    this.utilisateurService.postUtilisateur(data).subscribe(
+      response => {
+        console.log('Données envoyées avec succès : ', response);
+        // Faire quelque chose après l'envoi des données...
+      },
+      error => {
+        console.error('Erreur lors de l\'envoi des données : ', error);
+        // Gérer l'erreur...
+      }
+    );
+  }
 }
