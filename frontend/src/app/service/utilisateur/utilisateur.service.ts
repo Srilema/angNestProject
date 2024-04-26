064dce598b0bb741f013c4fd64802501cad64466
elodie ALB <elodie.albares0201@gmail.com>
2024-04-26 18:32:58 +0200

    add methode

---
+++ frontend/src/app/service/utilisateur/utilisateur.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilisateurService {
+
+  constructor(private http:HttpClient) { }
+
+  getUtilisateurs()
+  {
+    return this.http.get('http://localhost:3100/utilisateurs').pipe(
+      map((data: any) => data as Array<any>)
+    );
+  }
+
+  postUtilisateur(data:any){
+    return this.http.post('http://localhost:3100/utilisateurs', data);
+  }
+}
