d6c42d956a48b8cbce97797928864a3b4141dd28
elodie ALB <elodie.albares0201@gmail.com>
2024-04-29 15:49:16 +0200

    add redirection

--- frontend/src/app/creation-compte/creation-compte.component.ts
+++ frontend/src/app/creation-compte/creation-compte.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UtilisateurService } from '../service/utilisateur/utilisateur.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-creation-compte',
@@ -8,7 +9,7 @@ import { UtilisateurService } from '../service/utilisateur/utilisateur.service';
 })
 export class CreationCompteComponent {
 
-  constructor(private utilisateurService: UtilisateurService){}
+  constructor(private utilisateurService: UtilisateurService,private router : Router ){}
 
   submit(data: any) {
     data.role="BAILLEUR";
@@ -16,12 +17,18 @@ export class CreationCompteComponent {
     this.utilisateurService.postUtilisateur(data).subscribe(
       response => {
         console.log('Données envoyées avec succès : ', response);
-        // Faire quelque chose après l'envoi des données...
+       
+        if (response) {
+          console.log('response : ', response);
+          // Naviguer vers une autre page si le formulaire est valide
+          this.router.navigate(['/connexion']);
+        }
       },
       error => {
         console.error('Erreur lors de l\'envoi des données : ', error);
         // Gérer l'erreur...
       }
     );
+    
   }
 }
